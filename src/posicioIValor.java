cad8f5f0869d9c8657eb78b2737abce01860392d
sillyck <ribellas2002@gmail.com>
2022-01-13 23:36:54 +0100

    commit

--- src/posicioIValor.java
+++ src/posicioIValor.java
@@ -1,23 +1,41 @@
 /**
  * @author Jordi Ribellas
  */
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class posicioIValor {
 
 	public static void main(String[] args) {
 		
 		int num[]=new int[10];
 		
-		System.out.print("Introduce un numero ");
-		
-		Scanner n1 = new Scanner(System.in);
-		int numero1 = n1.nextInt();
-
+		demanarNumeros(num);
 		
 	}
 	
 	
-	for(int i=0; i) {
+	public static void demanarNumeros(int num[]) {
+		
+		for(int i=0; i<10; i++) {
 			
+			String n = JOptionPane.showInputDialog("Quin numero vols introduir a la posició " + (i+1));
+			int numero1 = Integer.parseInt(n);
+			
+			num[i] = numero1;
+		}
+		
+		llistarNumeros(num);
+		
+	}
+	
+	public static void llistarNumeros(int num[]) {
+		
+		System.out.println("Posició - Valor");
+		
+		for(int i=0;i<10;i++) {
+			
+			System.out.println((i+1) + " - " + num[i]);
+			
+		}
+		
 	}
 }
